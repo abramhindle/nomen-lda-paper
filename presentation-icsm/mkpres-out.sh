ebb41e33a1e89c71714d78834e7396dd1f34c553
abez <abez@reptile.abez.ca>
2009-09-22 16:20:47 -0400

    more changes

--- presentation-icsm/mkpres-out.sh
+++ presentation-icsm/mkpres-out.sh
@@ -1,4 +1,5 @@
-rm -rf pres-out
+rm -rf pres-out/*.png
 mkdir pres-out
 awk '{print "convert -composite  white.png " $1 " pres-out/" (sprintf("%003d",FNR)) ".png"}' < order  | sh -x
+#awk '{print "cp " $1 " pres-out/" (sprintf("%003d",FNR)) ".pdf"}' < order  | sh -x
 
