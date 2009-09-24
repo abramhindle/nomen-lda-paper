2ef81d533a490076ac0627bbf62d89cff59396ab
abez <abez@reptile.abez.ca>
2009-09-24 02:27:07 -0400

    more changes

--- presentation-icsm/mkpres-out-pdf.sh
+++ presentation-icsm/mkpres-out-pdf.sh
@@ -3,3 +3,4 @@ mkdir pres-out
 #awk '{print "convert -composite  white.png " $1 " pres-out/" (sprintf("%003d",FNR)) ".png"}' < order  | sh -x
 awk '{print "cp " $1 " pres-out/" (sprintf("%003d",FNR)) ".pdf"}' < order  | sh -x
 pdfjoin --outfile pres1.pdf pres-out/*.pdf
+cp pres1.pdf icsm2009-presentation-ahindle.pdf
