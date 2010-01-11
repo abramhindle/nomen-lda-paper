7b3145ce617858baa68f7a39c9b0aacd1c7b216a
abez <abram.hindle@softwareprocess.us>
2010-01-10 21:22:16 -0500

    Modified the python files check_validity.py and split_periods.py to work with mysql as well ln -s ../../data . for src/validate/ we'll need to fix up the paths..

--- src/validate/split_periods.py
+++ src/validate/split_periods.py
@@ -1,10 +1,33 @@
-in_file = 'data/maxdb-lda.report2.xml'
-out_file = 'data/split/'
-xsl_file = 'split.xsl'
+#in_file = 'data/maxdb-lda.report2.xml'
+in_file = 'annotations/mysql-3.23/mysql-3.23.lda.report.xml'
+#out_file = 'data/split/'
+out_file = 'data/mysql-tagged'
+#xsl_file = 'split.xsl'
         
 import lxml.etree
+doc = lxml.etree.parse(in_file)
+periods = doc.findall('Period')
+for period in periods:
+    time = period.attrib['time']
+    time_file = out_file + "/" + time + "xml"
+    # period.write( time_file, encoding='UTF-8' )
+    tfile = open( time_file,"w" )
+    tfile.write( lxml.etree.tostring( period, pretty_print = True ) )
+    tfile.close()
+
+exit(0)
+
+in_file = 'data/maxdb-lda.report2.xml'
+#in_file = 'annotations/mysql-3.23/mysql-3.23.lda.report.xml'
+out_file = 'data/split/'
+#out_file = 'data/mysql-tagged'
+xsl_file = 'split.xsl'
+        
+
+
+# ok look I think I'm missing the XSLT for this
 styledoc = lxml.etree.parse(xsl_file)
 transform = lxml.etree.XSLT(styledoc)
 doc = lxml.etree.parse(in_file)
 result_tree = transform(doc)
-result_tree.write(out_file+, encoding='UTF-8')  
+#result_tree.write(out_file+, encoding='UTF-8')  
