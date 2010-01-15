84124d872167a904294b59cd26830a6f78bbea9c
abez <abez@reptile.abez.ca>
2010-01-14 21:33:23 -0500

    Adding stuff to allow for stats about TP FP etc.

--- src/validate/check_validity.py
+++ src/validate/check_validity.py
@@ -1,7 +1,15 @@
 # Suite of functions to parse word lists
+import get_exps
+from collections import defaultdict
+
+def make_count():
+    return defaultdict(int)
+
 
 # As far as I can tell this script is meant to see if
 # the annotations match or don't match portability efficiency reliability etc..
+
+
 
 def load_period(period, data_dir = "/Users/nernst/Documents/papers/current-papers/abram/naming-paper/data/maxdb-tagged/"): 
     """ load the xml file with the data """
@@ -13,14 +21,13 @@ def load_period(period, data_dir = "/Users/nernst/Documents/papers/current-paper
     #spme test establishing correct number
     return topics
     
-def load_wordlists(): 
+def load_wordlists(expdir = '../exp3/' ): 
     """ load the datafile with the wordlists we want to use """
-    exp = '../exp3/'
     quality_map = {'portability':[], 'efficiency':[], 'reliability':[], 'functionality':[], 'usability':[], 'maintainability':[]}
     for q in quality_map.keys():
-        q_file = open(exp + 'wordlist.' + q)
+        q_file = open( expdir + 'wordlist.' + q )
         for line in q_file:
-            quality_map[q].append(line.rstrip())
+            quality_map[ q ].append( line.rstrip() )
     return quality_map
     
 def create_element_list(topic):
@@ -41,18 +48,24 @@ def create_element_list(topic):
         annotations.append(a.attrib['name'])
     return annotations, thresh_el
     
-def index_matching_wordlists(thresh_el, annotations, quality_map):
+def index_matching_wordlists( thresh_el, annotations, quality_map ):
     """"""
     el_results = {}
     anno_results = []
     count = 0
+    # for each non functional requirement
+    matching_words = []
     for quality in quality_map.keys():
+        count = 0
+        # for each word in the quality map
         for keyword in quality_map[quality]:
+            #
             if keyword in thresh_el:
+                matching_words.append( (quality, keyword) )
                 count = count + 1 #count of occurences for that quality's keywords
         # assign the count to that quality
         el_results[quality] = count
-        count = 0
+
     matches = [x for x in el_results.keys() if el_results[x] > 0]  # only print matches   
     overlap = False      
     # this was a little hard to read
@@ -61,21 +74,30 @@ def index_matching_wordlists(thresh_el, annotations, quality_map):
     # We want to check if there no annotations or it was annotated NONE
     # then we want to check if matches overlaps ok?
     # i have no clue why none and zip for annotations means anything
+    anno_matches = [m for m in matches if m in annotations]
+    nanno_matches = len( anno_matches )
+    nannos = 0
     if (len(annotations) == 0):
         overlap = True
+        nanno_matches = 0
     elif ("none" in annotations):
         overlap = True
+        nanno_matches = 0
     else:
-        overlap = len([m for m in matches if m in annotations]) > 0
+        nannos = len( annotations )
+        overlap = len( anno_matches ) > 0
 
     #for m in matches:
     #    if m in annotations or (len(annotations) > 0 and annotations[0] == 'none'):
     #        overlap = True
     # the script used to print here.
-    if overlap:
-        return 'yes'
-    else:
-        return "no match"
+
+    return overlap, (anno_matches, matches, matching_words), ( nanno_matches , nanno_matches )
+
+    # if overlap:
+    #     return 'yes'
+    # else:
+    #     return "no match"
 
 def compare_results():
     """"""
@@ -87,33 +109,49 @@ def compare_results():
 # load up periods per project
 # and the write out if a project
 if __name__ == '__main__':
-    maxdb_periods = ["1088563753", "1098931753",   "1119667753",  "1132627753",  "1142995753", 
-                    "1091155753",  "1101523753",  "1122259753",  "1135219753",  "1145587753", 
-                    "1093747753",  "1104115753",  "1124851753",  "1137811753",  "1148179753", 
-                    "1096339753",  "1106707753",  "1130035753",  "1140403753",  "1150771753"]
-    mysql_periods = ["965099404" ,"967691404" ,"970283404" ,"972875404" ,"975467404" ,"978059404" ,"980651404" ,"983243404" ,"985835404" ,"988427404" ,"991019404" ,"993611404" ,"996203404" ,"998795404" ,"1001387404" ,"1003979404" ,"1006571404" ,"1009163404" ,"1011755404" ,"1014347404" ,"1016939404" ,"1019531404" ,"1022123404" ,"1024715404" ,"1027307404" ,"1029899404" ,"1032491404" ,"1035083404" ,"1037675404" ,"1040267404" ,"1042859404" ,"1045451404" ,"1048043404" ,"1050635404" ,"1053227404" ,"1055819404" ,"1058411404" ,"1061003404" ,"1063595404" ,"1066187404" ,"1068779404" ,"1071371404" ,"1073963404" ,"1076555404" ,"1079147404" ,"1081739404" ,"1084331404" ,"1086923404" ,"1089515404" ,"1092107404"]
-    maxdb_dir = "data/maxdb-tagged/"
-    mysql_dir = "data/mysql-tagged/"
-    exps = [ ["maxdb",maxdb_dir, maxdb_periods] , [ "mysql",mysql_dir, mysql_periods ] ]
+    exps = get_exps.get_exps() 
     # test_period = '1098931753'
     #     topics = load_period(test_period)
-    quality_map = load_wordlists()
-    for exp in exps:
-        dir = exp[1]
-        periods = exp[2]
-        report = exp[0]
-        report_file = report + ".report.txt"
-        rfile = open( report_file, "w" )
-        print report
-        rfile.write(report+'\n')
-        for period in periods:
-          pstr = "Period #" + period
-          print pstr
-          rfile.write(pstr+'\n')
-          topics = load_period(period, data_dir = dir)
-          for t in topics:
-              annotations, thresh_el = create_element_list(t)
-              res = index_matching_wordlists(thresh_el, annotations, quality_map)
-              print res
-              rfile.write(res+'\n')
-        rfile.close()
+    word_exps = get_exps.word_list_exps()
+    for wl in word_exps:
+        wld = get_exps.word_list_dir( wl )
+        print wl
+        quality_map = load_wordlists( wld )
+        
+        for exp in exps:
+            ddir = exp[1]
+            periods = exp[2]
+            report = exp[0]
+            report_file = wl + "." + report + ".report.txt"
+            rfile = open( report_file, "w" )
+            print report
+            rfile.write(report+'\n')
+            tp = make_count() #true positive  - matches but is
+            fp = make_count() #false positive - matches but isn't
+            fn = make_count() #false negative - doesn't match but does
+            tn = make_count() #true negative  - doesn't match but isn't
+            
+            for period in periods:
+                pstr = "Period #" + period
+                print pstr
+                rfile.write(pstr+'\n')
+                topics = load_period(period, data_dir = ddir)
+                for t in topics:
+                    annotations, thresh_el = create_element_list(t)
+                    res, _, stats = index_matching_wordlists(thresh_el, annotations, quality_map)
+                    anno_matches = ma[0]
+                    matches = ma[1] #matching qualities
+                    matching_words = ma[2]
+                    # left here
+                    # tp
+                    for a in anno_matches:
+                        tp[a] += 1
+                    #for a in matches:
+                        
+                    ress = "Yes"
+                    if not res:
+                        ress = "No"
+                    print res
+                    rfile.write(ress+'\n')
+            # 
+            rfile.close()
