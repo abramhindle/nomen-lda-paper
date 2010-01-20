0b5546791d32c45ec74388a5266d6e563322dd47
abez <abram.hindle@softwareprocess.us>
2010-01-20 01:57:24 -0500

    Added maxdb__mysql combined results

--- src/validate/weka-shrink.sh
+++ src/validate/weka-shrink.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # note: this file is for shrinking the huge files that weka generates
-for file in output/maxdb.*.NaiveBayes.txt output/mysql.*.NaiveBayes.txt output/mysql.*.SMO.txt output/maxdb.*.SMO.txt 
+for file in output/*.*.NaiveBayes.txt output/*.*.SMO.txt 
 do
 	tail -n 55 $file > /tmp/tailed
 	cp /tmp/tailed $file
