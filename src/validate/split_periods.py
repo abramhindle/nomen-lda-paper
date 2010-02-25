a19c5c67eb2cec19e450d4b829406820c21dfc7e
abez <abram.hindle@softwareprocess.us>
2010-02-25 00:12:42 -0500

    Sorry bro, postgresql data!

--- src/validate/split_periods.py
+++ src/validate/split_periods.py
@@ -1,3 +1,30 @@
+
+#in_file = 'data/maxdb-lda.report2.xml'
+in_file = 'annotations/postgresql/postgresql.lda.report.xml'
+#out_file = 'data/split/'
+out_file = 'data/postgresql-tagged'
+#xsl_file = 'split.xsl'
+        
+import lxml.etree
+doc = lxml.etree.parse(in_file)
+periods = doc.findall('Period')
+for period in periods:
+    time = period.attrib['time']
+    time_file = out_file + "/" + time + "xml"
+    # period.write( time_file, encoding='UTF-8' )
+    tfile = open( time_file,"w" )
+    tfile.write( lxml.etree.tostring( period, pretty_print = True ) )
+    tfile.close()
+
+exit(0)
+
+
+
+
+
+
+# for mysql
+
 #in_file = 'data/maxdb-lda.report2.xml'
 in_file = 'annotations/mysql-3.23/mysql-3.23.lda.report.xml'
 #out_file = 'data/split/'
