5bbf58dae20bc47c7e17ca3e00a8fd49738b6134
Neil Ernst <neil@neilernst.net>
2011-01-13 16:44:33 -0500

    removing ICSM files for space
    Signed-off-by: Neil Ernst <neil@neilernst.net>

--- icsm09/dist.sh
+++
@@ -1,9 +0,0 @@
-#make clean paper1.pdf
-#FILE=indentation.paper1.`date +"%Y%m%d.%H%M%S.%h.%d.%Y"`.pdf
-#cp paper1.pdf $FILE
-#vex $FILE
-FILEO=lda-paper.pdf
-make clean $FILEO 
-FILE=lda-paper.`date +"%Y%m%d.%H%M%S.%h.%d.%Y"`.pdf
-cp $FILEO $FILE
-vex $FILE
