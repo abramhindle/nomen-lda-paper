8e18f9b3c820872235c5e568d1b8f7f3cc1f3e37
Abram Hindle <abram.hindle@softwareprocess.es>
2011-05-16 14:39:51 -0700

    Presentation updates Signed-off-by: Abram Hindle <abram.hindle@softwareprocess.es>

--- msr/presentation/slide-number-of.sh
+++ msr/presentation/slide-number-of.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-FILE=`basename $1 | sed -e 's/\.pdf//'`
+FILE=`basename $1 | sed -e 's/\.[a-zA-Z]*$//'`
 find order -type f -or -type l | sort -n | awk '{print NR " " $0}' | fgrep "$FILE" | awk '{print $1}'
