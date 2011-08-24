d5e765c6b31bd569cb42d2d25859e3f770ef4304
Abram Hindle <abram.hindle@softwareprocess.es>
2011-08-23 19:25:59 -0700

    updated the classifier list and the trails

--- src/validate/run-weka-classifier-single-trials.sh
+++ src/validate/run-weka-classifier-single-trials.sh
@@ -2,6 +2,7 @@
 # We make experimental make files
 # We run each annotation against each classifier
 CPUS=4
+perl make-single-class-arff-files.pl pgsql && make -f Makefile.pgsql -j $CPUS all
 perl make-single-class-arff-files.pl mysql && make -f Makefile.mysql -j $CPUS all
 perl make-single-class-arff-files.pl maxdb && make -f Makefile.maxdb -j $CPUS all
 perl make-single-class-arff-files.pl maxdb__mysql && make -f Makefile.maxdb__mysql -j $CPUS all
