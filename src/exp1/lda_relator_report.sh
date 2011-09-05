967ff1da13dc91f52240201ffc944070caf4fd1e
Abram Hindle <abram.hindle@softwareprocess.es>
2011-09-05 15:54:27 -0700

    exp1 pgsql resultsd

--- src/exp1/lda_relator_report.sh
+++ src/exp1/lda_relator_report.sh
@@ -1,4 +1,5 @@
-for proj in mysql maxdb
+#for proj in mysql maxdb pgsql
+for proj in pgsql
 do
 	cp ../counts/$proj.lda.report ./lda.report
 	~/projects/hiraldo_grok/hs/Lda_relator  > $proj.lda_relator.report
